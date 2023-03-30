d8d52e0dab7dececd4651dc85a4a8c42ec60507b
HairulBahri11 <hairulb876@gmail.com>
2023-03-30 21:44:15 +0700

    update data

--- TUGAS 15 - OOP/tugas.php
+++ TUGAS 15 - OOP/tugas.php
@@ -1,16 +1,5 @@
 <?php
-// Lengkapi file Pegawai. php sebagai berikut:
 
-// setTunjab = 20% dari Gaji Pokok
-// setTunkel= 10% dari Gaji Pokok untuk sudah menikah (ternary)
-// setZakatProfesi= 2,5% dari GajiPokok untuk muslim dan Gaji Kotor minimal 6jt
-// mencetak => nip, nama, jabatan, agama, status, Gaji Pokok, Tunj Jab, Tunkel, Zakat, Gaji Bersih 
-
-
-// Buatlah objPegawai dengan data:
-
-// 5 instance object pegawai
-// cetaklah semua struktur gaji pegawai
 class pegawai
 {
     private $nama;
